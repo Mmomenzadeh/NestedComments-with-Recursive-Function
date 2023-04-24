9c205a5795730a69469f0ef36a5d6faa1a49e2ec
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-24 14:55:13 +0430

    refactor : The comments array became modular

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,4 @@
-const comments = [
-  {
-    name: "Ashton",
-    text: "He made a brief comment. She gave positive comments about the dinner.Jane responded poorly to the negative comment.A teacher can help students by providing a lot of helpful comments",
-    children: [
-      {
-        name: "Robert",
-        text: "It was this that saved her from some snide comment about her less-than-fashionable clothes.",
-        children: [
-          {
-            name: "Ayden",
-            text: "To comment or make suggestions on these or other ISO standards on polymer melt rheology please contact Martin Rides.",
-            chlidren: [],
-          },
-        ],
-      },
-      {
-        name: "Hayden",
-        text: "It was also used by a class of bards or itinerant soothsayers known by the name of vates, of whom the most famous was one Marcius, and in the verses, as sung at harvest-homes and weddings, which gave expression to the coarse gaiety of the people and to their strong tendency to personal raillery and satiric comment.",
-        chlidren: [],
-      },
-    ],
-  },
-  {
-    name: "Alis",
-    text: "He frantically thought about how to explain his comment.",
-    children: [],
-  },
-];
+import comments from "./Comments.js";
 
 const renderComments = (comments) => {
   let output = "";
