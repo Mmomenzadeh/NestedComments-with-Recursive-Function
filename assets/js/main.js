9ef308d3df229cfaf671a6e24a558f102eec6a4f
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-04-24 16:11:57 +0430

    perf : added comment for code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
 import comments from "./Comments.js";
 
 const renderComments = (comments) => {
+  // HTML output to be added to the page
   let output = "";
   for (const comment of comments) {
+    // create p , h3 elemant for each name and txt
     let name = `<h4>${comment.name}</h4>`;
     let text = `<p>${comment.text}</p>`;
 
     let children = "";
+    // use Recursive function for show childern comments
     if (Array.isArray(comment.children) && comment.children.length > 0) {
       children = renderComments(comment.children);
     }
